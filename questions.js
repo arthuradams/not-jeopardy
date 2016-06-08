c71d3a336672606341d3b0f10dd61883d6f3db5f
Arthur Adams <arthuradams@gmail.com>
2016-06-07 21:25:14 -0400

    add file of questions, uodate index.hrml to handle it, add sample gif

---
+++ questions.js
@@ -0,0 +1,24 @@
+var questions =  [
+  {
+    text : 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?',
+    answers : [ { text: 'A woodchuck would chuck as much wood etc etc etc',
+                  correct: true },
+                  { text: 'bleen'  },
+                  { text: 'forty-two' },
+                  { text: 'pi' }
+              ],
+    image : "woodchuck.jpg",
+  },
+
+  {
+    text : 'Who put the bob in the bop-she-bop?',
+    answers : [ { text: 'Batman',
+                  correct: true },
+                  { text: 'Richard Nixon'  },
+                  { text: 'Richard Nixon, dressed as Batman' },
+                  { text: 'Batman, dressed as Richard Nixon' }
+              ],
+    image : "woodchuck.jpg",
+  },
+
+]
