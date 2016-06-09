1a910352e8f6926c8f5f60c8cea23b65e62bb951
Arthur Adams <arthuradams@gmail.com>
2016-06-09 09:58:59 -0400

    go to new question after input

--- trivia.js
+++ trivia.js
@@ -1,4 +1,21 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 function displayQuestion( question ) {
   console.log(question.text);
@@ -7,7 +24,7 @@ function displayQuestion( question ) {
 
 function displayAnswers( question ) {
 
-  var answersArray = question.answers;
+  var answersArray = shuffle( question.answers );
   var selectors = ["A", "B", "C", "D"];
   var correctAnswer;
 
@@ -42,26 +59,80 @@ function displayResult( choice, correct ) {
   }
 }
 
+function updateAll(score,result,question) {
+  $("#result").text(result);
+  $("#score").text(score);
+  displayQuestion( question );
+  return displayAnswers( question );
+}
+
+function nextQuestion(count) {
+  if (count < questions.length) {
+    return questions[count];
+  }
+  else {
+    return questions[questions.length - 1];
+  }
+}
 
 $(document).ready(function(){
 
+  var score = 0;
+  var correctAnswer;
+  var result;
+  var questionCount = 0;
+
+  correctAnswer = updateAll(score,"Welcome!",questions[questionCount++]);
+/*  $("#score").text(score);
   displayQuestion( questions[0] );
-  var correctAnswer = displayAnswers( questions[0] );
+  correctAnswer = displayAnswers( questions[0] ); */
 
   $("#answerA").on("click", function(){
-    displayResult("A", correctAnswer );
+    if (correctAnswer == "A") {
+      score += 1;
+      result = "Your previous answer was right!";
+    }
+    else {
+      score -= 1;
+      result = "Your previous answer was wrong!";
+    }
+    correctAnswer = updateAll(score,result,nextQuestion(questionCount++));
   });
 
   $("#answerB").on("click", function(){
-    displayResult("B", correctAnswer );
+    if (correctAnswer == "B") {
+      score += 1;
+      result = "Your previous answer was right!";
+    }
+    else {
+      score -= 1;
+      result = "Your previous answer was wrong!";
+    }
+    correctAnswer = updateAll(score,result,nextQuestion(questionCount++));
   });
 
   $("#answerC").on("click", function(){
-    displayResult("C", correctAnswer );
+    if (correctAnswer == "C") {
+      score += 1;
+      result = "Your previous answer was right!";
+    }
+    else {
+      score -= 1;
+      result = "Your previous answer was wrong!";
+    }
+    correctAnswer = updateAll(score,result,nextQuestion(questionCount++));
   });
 
   $("#answerD").on("click", function(){
-    displayResult("D", correctAnswer );
+    if (correctAnswer == "D") {
+      score += 1;
+      result = "Your previous answer was right!";
+    }
+    else {
+      score -= 1;
+      result = "Your previous answer was wrong!";
+    }
+    correctAnswer = updateAll(score,result,nextQuestion(questionCount++));
   });
 
 /*  $("#answerA").on("click", checkAnswer('A'));
